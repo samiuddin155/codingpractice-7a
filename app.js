0967168b05a6d941078337a1c2b684caae4a4a77
samiuddin155 <117622773+samiuddin155@users.noreply.github.com>
2023-05-16 17:42:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const {open} = require("sqlite");
 const sqlite3 = require("sqlite3");
 const app = express()
+app.use(express.json())
 
 
 const dbPath = path.join(__dirname, "cricketMatchDetails.db");
